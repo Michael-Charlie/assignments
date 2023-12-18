8b4232391a3df00fc039fbae1a939a886ba51d1e
michael charlie <michael.charlie.github@gmail.com>
2023-12-18 12:24:05 -0800

    level 6 stuff pt 2

---
+++ level-6/assignments/rtv-pt-2/models/Issue.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const issueSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    datePosted: {
+        type: Date,
+        default: Date.now
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    }
+})
+module.exports = mongoose.model("Issue", issueSchema)
